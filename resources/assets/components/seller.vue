1445f986ca4dd2573647f95ae3ac403a8afef8b9
matt trask <mjftrask@gmail.com>
2018-02-18 10:23:37 -0600

    remove # symbol and description

--- resources/assets/components/seller.vue
+++ resources/assets/components/seller.vue
@@ -23,8 +23,7 @@
                             <h3>{{ post.title }}</h3>
                             <p>Seller: {{ post.seller }}</p>
                             <p>Posted: {{ post.posted | date }}</p>
-                            <p>{{ post.description }}</p>
-                            <a v-bind:href="'/#/posts/' + post.id"  class="btn btn-success" role="button">View Post</a>
+                            <a v-bind:href="'/posts/' + post.id"  class="btn btn-success" role="button">View Post</a>
                         </div>
                     </div>
                 </div>
